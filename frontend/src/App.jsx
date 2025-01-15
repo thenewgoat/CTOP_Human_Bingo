915fc9f7f064592b2eddde2b6cf72577d85f0e11
Xinyang <zhangxinyang888@gmail.com>
2025-01-15 09:04:18 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
+
 import { createPlayer, getBingoSheets, signTrait } from './api';
 
 const App = () => {
