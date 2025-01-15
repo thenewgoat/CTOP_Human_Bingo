d8132cf51b469ac97e5ff466842fc8b8a7a3aa16
Xinyang <zhangxinyang888@gmail.com>
2025-01-15 20:10:55 +0800

    Update gameRoutes.js
    typo

--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../db");
-const authenticateToken = require("../middleware/authenticateToken");
+const authenticateToken = require("../middleware/authenticateTokens");
 
 // Retrieve a bingo sheet for a player
 router.get("/:playerId", authenticateToken, async (req, res) => {
