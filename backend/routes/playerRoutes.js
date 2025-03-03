dc83623f6cff8ca3500d89cfedef3b5adeaa35ec
Xinyang <zhangxinyang888@gmail.com>
2025-03-04 01:39:28 +0800

    Update playerRoutes.js
    ok actually removed the testing code, this is the actual fix

--- backend/routes/playerRoutes.js
+++ backend/routes/playerRoutes.js
@@ -74,12 +74,6 @@ router.post("/", async (req, res) => {
       const shuffledTraits = [...TRAITS_POOL].sort(() => 0.5 - Math.random());
       const selectedTraits = shuffledTraits.slice(0, 25);
 
-      const testTrait = "Can solve a Rubik's Cube";
-      const boxInsertValue = `(${bingoSheetId}, '${testTrait}')`;
-      const testQuery = `INSERT INTO bingo_boxes (bingo_sheet_id, trait) VALUES ${boxInsertValue}`;
-      console.log("Test Query:", testQuery);
-      await pool.query(testQuery);
-
       // Insert the boxes into the bingo_boxes table
       const boxInsertValues = selectedTraits
         .map((trait) => `(${bingoSheetId}, '${trait.replace(/'/g, "''")}')`)
